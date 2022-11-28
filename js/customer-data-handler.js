5033c3b1571d47bdf67b33208316a14deb703ee7
BranJ <bkaddu12@gmail.com>
2022-11-27 18:08:54 -0800

    Payment and shipping page is now Functional
    Clicking "Continue to checkout" saves data input and redirects to checkout page.

--- js/customer-data-handler.js
+++ js/customer-data-handler.js
@@ -1,22 +1,66 @@
-let customer = class
+let customer =
 {
-    constructor(customer_name,customer_telephone,customer_email,customer_address,payment_info)
-    {
-        this.customer_name = customer_name;
-        this.customer_telephone = customer_telephone;
-        this.customer_email = customer_email;
-        this.customer_address = customer_address;
-        this.payment_info = payment_info;
-    }
 
-    submitForm(customer_name,customer_telephone,customer_email,customer_address,payment_info) //save customer information for next page or for order
-    {
-        let temp = new customer(customer_name,customer_telephone,customer_email,customer_address,payment_info);
-        sessionStorage.setItem("customer",JSON.stringify(temp));
-    }
+    customer_name: "",
+    customer_telephone: "",
+    customer_email: "",
+    customer_address: "",
+    customer_city: "",
+    customer_state: "",
+    customer_zip: "",
 
-    readForm()            //return customer information
-    {
-        return JSON.parse(sessionStorage("customer"));
-    }
+}
+
+function constructCust(customer_name,customer_telephone,customer_email,customer_address,customer_city,customer_state,customer_zip)
+{
+    customer.customer_name = customer_name;
+    customer.customer_telephone = customer_telephone;
+    customer.customer_email = customer_email;
+    customer.customer_address = customer_address;
+    customer.customer_city = customer_city;
+    customer.customer_state = customer_state;
+    customer.customer_zip = customer_zip;
+}
+
+let payment =
+{
+
+        card_name: "",
+        card_num:   "",
+        exp_month:   "",
+        cvv: "",
+        exp_year: "",
+
+}
+
+function constructPay(card_name,card_num,exp_month,cvv,exp_year)
+{
+    payment.card_name = card_name;
+    payment.card_num = card_num;
+    payment.exp_month = exp_month;
+    payment.cvv = cvv;
+    payment.exp_year = exp_year;
+}
+
+function submitForm(customer_data, payment_data) //save customer information for next page or for order
+{
+    let temp1 = customer;
+    let temp2 = payment;
+    console.log(customer);
+    console.log(JSON.stringify(temp1));
+    sessionStorage.setItem("customer",JSON.stringify(temp1));
+    sessionStorage.setItem("payment", JSON.stringify(temp2));
+}
+
+function readCustomer()            //return customer information
+{
+    let temp = sessionStorage.getItem("customer");
+    console.log(temp);
+    return JSON.parse(temp);
+}
+
+function readPayment()
+{
+    let temp = sessionStorage.getItem("payment")
+    return JSON.parse(temp);
 }
